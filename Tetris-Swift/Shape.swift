84ea2b958ea08a1208a3e40f8ca49b56648c6d1c
Sergio G <sergiorodriguezgijon@gmail.com>
2015-01-24 23:41:31 +0100

    Orientation.

---
+++ Tetris-Swift/Shape.swift
@@ -0,0 +1,42 @@
+//
+//  Shape.swift
+//  Tetris-Swift
+//
+//  Created by Sergio Gijón on 24/1/15.
+//  Copyright (c) 2015 Sergio Gijón. All rights reserved.
+//
+
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, Printable {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+          case .Zero:
+            return "0"
+          case .Ninety:
+            return "90"
+          case .OneEighty:
+            return "180"
+          case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation(rawValue: Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+    static func rotate(orientation:Orientation, clockwise: Bool) -> Orientation {
+        var rotated = orientation.rawValue + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.rawValue {
+            rotated = Orientation.Zero.rawValue
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        return Orientation(rawValue: rotated)!
+    }
+}
