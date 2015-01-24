ab2d5fd216c37483b83ad3811abd6a0e6fc7bec4
Sergio G <sergiorodriguezgijon@gmail.com>
2015-01-24 22:46:57 +0100

    Block colors.

---
+++ Block.swift
@@ -0,0 +1,40 @@
+//
+//  Block.swift
+//  Tetris-Swift
+//
+//  Created by Sergio Gijón on 24/1/15.
+//  Copyright (c) 2015 Sergio Gijón. All rights reserved.
+//
+
+import SpriteKit
+
+let NumberOfColors: UInt32 = 6
+
+enum BlockColor: Int, Printable {
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    var spriteName : String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    var description: String {
+        return self.spriteName
+    }
+    
+    static func random() -> BlockColor {
+        return BlockColor(rawValue: Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
