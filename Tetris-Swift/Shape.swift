a20052b3dbd21ce688c54c4d38efe16ad7f565db
Sergio G <sergiorodriguezgijon@gmail.com>
2015-01-25 00:04:18 +0100

    Shape: Part I.

--- Tetris-Swift/Shape.swift
+++ Tetris-Swift/Shape.swift
@@ -40,3 +40,93 @@ enum Orientation: Int, Printable {
         return Orientation(rawValue: rotated)!
     }
 }
+
+let NumShapesTypes: UInt32 = 7
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, Printable {
+    let color: BlockColor
+    
+    var blocks = Array<Block>()
+    var orientation: Orientation
+    
+    var column, row: Int
+    
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+    
+    var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [:]
+    }
+    
+    var bottomBlocks:Array<Block> {
+        if let bottomBlocks = bottomBlocksForOrientations[orientation] {
+           return bottomBlocks
+        }
+        return []
+    }
+    
+    var hashValue:Int {
+        return reduce(blocks, 0) { $0.hashValue ^ $1.hashValue }
+    }
+    
+    var description: String {
+        return "\(color) \(orientation): <\(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])>"
+    }
+    
+    init(column:Int, row:Int, color: BlockColor, orientation: Orientation){
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    convenience init(column:Int, row:Int) {
+        self.init(column: column, row:row, color: BlockColor.random(), orientation:Orientation.random())
+    }
+}
+
+func ==(left:Shape, right:Shape) -> Bool {
+    return left.description == right.description
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
